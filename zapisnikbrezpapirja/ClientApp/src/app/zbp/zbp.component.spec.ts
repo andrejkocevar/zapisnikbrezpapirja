7743bc9ed153f38c9f25941e4cedb7c1fe2bda94
andrejk <andrej.kocevar@rcg.si>
2020-02-04 14:53:02 +0100

    Add project files.

---
+++ zapisnikbrezpapirja/ClientApp/src/app/zbp/zbp.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ZbpComponent } from './zbp.component';
+
+let component: ZbpComponent;
+let fixture: ComponentFixture<ZbpComponent>;
+
+describe('zbp component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ZbpComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ZbpComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ zapisnikbrezpapirja/ClientApp/src/app/zbp/zbp.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-zbp',
+    templateUrl: './zbp.component.html',
+    styleUrls: ['./zbp.component.scss']
+})
+/** zbp component*/
+export class ZbpComponent {
+    /** zbp ctor */
+    constructor() {
+
+    }
+}
